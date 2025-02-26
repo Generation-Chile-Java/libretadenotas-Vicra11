48a9344cc67e9b383dcdb307169618564f4f6739
Vicra11 <vicenterojasg@gmail.com>
2025-02-26 20:22:32 -0300

    enviar

--- EvaluadorDeClases/src/LibretaDeNotas.java
+++ EvaluadorDeClases/src/LibretaDeNotas.java
@@ -1,12 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+public class LibretaDeNotas {
 
+    Map<String, ArrayList<>> EquipoPokemon = new HashMap<>();
 
-    }
 }
+
 
 /*
 
